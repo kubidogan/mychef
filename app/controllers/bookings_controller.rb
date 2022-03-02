2a49832624e797ebcb533d04746543c5ba835e3d
Emma Cheng <emmayauyau@gmail.com>
2022-03-02 18:07:51 +0000

    added booking controller, routes, and view page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,19 +4,22 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(review_params)
-    @review.reviewee = current_user
-    @review.reviewer = User.find(params[:user_id])
-    if @review.save
-      redirect_to chef_path(@user)
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.event = Event.find(params[:event_id])
+    if @booking.save
+      redirect_to booking_confirmation_path(@event)
     else
       render :new
     end
   end
 
+  def confirmation
+    @booking = Booking.find(params[:booking_id])
+
   private
 
   def booking_params
-    params.require(:review).permit(:rating, :comment, :reviewer_id, :reviewee_id)
+    params.require(:booking).permit(:user_id, :event_id)
   end
 end
