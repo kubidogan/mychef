71ad19022ba5b858a691835441157745f3276a4c
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-02 12:21:18 +0000

    app up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/myprofile', to: 'users#myprofile'
+  get '/myprofile/experiences/new', to: 'resumes#new'
+  post '/myprofile/experiences', to: 'resumes#create'
+  get '/events/:event_id/new', to: 'bookings#new'
+  post '/events/:event_id', to: 'bookings#create'
+  get '/chefs/:chef_id/review/new', to: 'reviews#new'
+  post '/chefs/:chef_id/review', to: 'reviews#create'
+
+  resources :events do
+    # resources :bookings, only: [:new, :create]
+  end
+
+
+  resources :users, only: [:index, :show], path: :chefs, as: :chefs, controller: :chefs do
+    member do
+      post :follow
+      post :unfollow
+    end
+  end
 end
