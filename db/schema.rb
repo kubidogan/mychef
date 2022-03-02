e0d1267133f761e29c897d669740de62b708912b
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-02 12:32:49 +0000

    seed file added

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 2022_03_02_114220) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username"
+    t.string "name"
+    t.string "cuisine"
+    t.string "address"
+    t.boolean "typeofuser"
+    t.boolean "work_status"
+    t.integer "price"
+    t.text "bio"
+    t.float "latitude"
+    t.float "longitude"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
