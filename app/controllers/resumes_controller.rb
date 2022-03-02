ff72f9c68eba4a47cbf7e104107331fc3a088d7b
Carolin Schmidt <mail@cdschmidt.com>
2022-03-02 16:11:24 +0100

    fixed resume section add

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,26 @@
 class ResumesController < ApplicationController
 
   def new
+    @resume = Resume.new
   end
 
   def create
+    @resume = Resume.new(resume_params)
+    @resume.user = current_user
+    if @resume.save!
+      redirect_to myprofile_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(
+      :experience,
+      :start_date,
+      :end_date,
+    )
   end
 end
