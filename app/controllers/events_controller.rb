0d6377cd822442df49a805ebd8a4990da7d7dd26
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-04 11:12:25 +0000

    created maps and added to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,20 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+    # @users = User.find(params[:id])
+    @events = Event.all
+    @users = User.all
+    @markers = @users.geocoded.map.each do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { user: user }),
+        image_url: helpers.asset_url("mapboxmarker.png")
+      }
+    end
+  end
+
   private
 
   def event_params
