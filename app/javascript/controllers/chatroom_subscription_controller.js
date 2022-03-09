2da955f7c37e07d01008e7d698d9de08a7a2ad32
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-09 13:38:36 +0000

    dm page fully working and styled, added to controller js

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -8,14 +8,24 @@ export default class extends Controller {
 
   connect() {
     console.log("connected to chat room")
+
+    const userId = this.messagesTarget.dataset.id
     this.channel = consumer.subscriptions.create(
-    { channel: "ChatroomChannel", id: this.chatroomIdValue },
-    { received: data => this.#insertMessageScrollDownAndResetForm(data) }
-  )
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.#insertMessageScrollDownAndResetForm(data, userId) }
+      )
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
-  #insertMessageScrollDownAndResetForm(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+  #insertMessageScrollDownAndResetForm(data, userId) {
+    const message = document.createElement("div")
+    message.innerHTML = data
+    if (message.firstChild.dataset.senderId === userId) {
+      message.firstChild.classList.add('sent-message');
+    } else {
+      message.firstChild.classList.add('received-message');
+    }
+    this.messagesTarget.insertAdjacentElement("beforeend", message)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
     this.formTarget.reset()
   }
