aa844e3855e82deba6e46c7cd0fe3285c5da78cd
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-12 16:34:05 +0000

    need to fix rating with if statement

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :reviewee, class_name: 'User', foreign_key: :reviewee_id
   belongs_to :reviewer, class_name: 'User', foreign_key: :reviewer_id
 
-  # validates :rating, length: { minimum: 1 }
+  # validates :rating, presence: { message: "must be given please" }
   # validates :comment, presence: true
 end
